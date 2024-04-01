aaa058455691793a47b48624631a6e8796a5e841
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-04-01 20:07:52 +0200

    [TASK] create a basic header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="py-5">
+      <div className="container mx-auto px-5 sm:px-0">
+        Header
+      </div>
+    </header>
+  );
+}
