5986c5e319106ab5e017d7b99098e508d99f232b
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-04-02 21:01:02 +0200

    [TASK] add headline in main

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="container mx-auto px-5 sm:px-0">
+      <h1 className="text-6xl font-bold uppercase">Blog Games</h1>
+    </main>
+  );
+}
