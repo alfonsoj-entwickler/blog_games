8428142c91eb39c674970699a4d5bfa60b12a226
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-04-04 22:02:42 +0200

    [UPDATE] header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,8 @@ export default function Header() {
   return (
     <header className="py-5">
       <div className="container mx-auto px-5 sm:px-0">
-        Header
+        <div className="navbar grid grid-cols-12"></div>
+        <nav></nav>
       </div>
     </header>
   );
