394a5d20c89a8187d7c26187e2246d8852208c35
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-05-24 19:15:54 +0200

    [TASK] updated card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,9 @@
 export default function Card() {
   return (
-    <div className="card">
-      <div className=""></div>
+    <div className="card relative w-fit h-fit font-work">
+      <div className="min-h-[320px]"></div>
       <div className="card-body"></div>
-      <div className=""></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent rounded-xl"></div>
     </div>
   );
 }
